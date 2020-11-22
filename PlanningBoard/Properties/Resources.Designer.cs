5edf8e8c9bc59f182cad9d974697b51d3c9233a1
Mokarrom Rahman <mrahman0000@gmail.com>
2020-11-22 12:44:11 -0700

    Changed project to WPF

--- PlanningBoard/Properties/Resources.Designer.cs
+++ PlanningBoard/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ResearchTracker.Properties
+namespace PlanningBoard.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace ResearchTracker.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ResearchTracker.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PlanningBoard.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- PlanningBoard/Properties/Settings.Designer.cs
+++ PlanningBoard/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ResearchTracker.Properties
+namespace PlanningBoard.Properties
 {
 
 
